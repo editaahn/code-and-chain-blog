23ab91e0ec2cd42f3585b8f2628aa678bd5425e5
Rita <editaahn@yahoo.com>
2025-09-23 22:34:14 +0900

    suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html>
+    <html suppressHydrationWarning>
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
